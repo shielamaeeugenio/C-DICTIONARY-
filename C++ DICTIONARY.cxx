919445c683428d3a881570804e46f3a695c0ad4e
shielamaeeugenio <eugenioshiela606@gmail.com>
2025-03-30 19:22:42 +0800

    Add files via upload

---
+++ C++ DICTIONARY.cxx
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string a;
+	
+	cout<<"Enter your Word:";
+	cin>>a;
+	
+	if (a== "ABACUS"){
+		cout<<"An early counting tool used for basic arithmetic.";
+		
+	}else if (a== "AREA"){
+		cout<<"The extent of a surface measured in m².";
+	}else if (a== "BUG"){
+		cout<<"Refers to a broken code with a fault or flaw.";
+	}else if (a== "PYTHON"){
+		cout<<"A programming language that is currently becoming more and more powerful.";
+	}else if (a== "COMPUTER PROGRAM"){
+		cout<<"A collection of codes that tell a computer what to do.";
+	}else if (a== "CAPACITY"){
+		cout<<"The volume of substance that a container will hold.";
+	}else if (a== "CODING"){
+		cout<<"How people create instructions for computers to follow.";
+	}else if (a== "CALCULUS"){
+		cout<<"The branch of mathematics involving derivatives and integrals.";
+	}else if (a== "INPUT"){
+		cout<<"Any interaction from the user to the program.";
+	}else if (a== "JAVA"){
+		cout<<"A powerful multi-platform programming language.";
+	}else if (a== "MASS"){
+		cout<<"A measure of the quantity of matter, independent of its volume.";
+	}else{
+		cout<<"No word is found";
+		
+	}
+	
+	return 0;
+	
+}
+		
+		
